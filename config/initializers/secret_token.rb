b26bdb406e45057d971169f1a09ede1fd2474d82
Carlos Salgado <casalgado86@yahoo.com>
2015-01-19 12:44:05 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Orden::Application.config.secret_key_base = 'ef60c6cad9bca91149d813606334d00bd6cc951df7bf0d3bd424b50628f38849a15d813581b4fad7659cae58439031955bf3c796d863299a38c54bb57b6ae094'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Orden::Application.config.session_store :cookie_store, key: '_orden_session'
