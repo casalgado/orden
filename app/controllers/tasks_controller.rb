7a5f147cf5297720d74f45b530b28c658d086167
Carlos Salgado <casalgado86@yahoo.com>
2015-01-20 08:26:27 -0500

    bootstrap25

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,7 +11,7 @@ class TasksController < ApplicationController
   def show
     @footer = false
     @task  = Task.new
-    @tasks = Task.all
+    @tasks = current_user.tasks
     #current_user.tasks.where(:due_date => Date.today.beginning_of_day..Date.today.end_of_day)
   end
 
