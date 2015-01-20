ee87c460523cd239064434585b0920e3843c32b4
Carlos Salgado <casalgado86@yahoo.com>
2015-01-20 13:15:18 -0500

    allow complete task 2

--- db/migrate/20150119180736_create_tasks.rb
+++ db/migrate/20150119180736_create_tasks.rb
@@ -2,9 +2,9 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.references :user, index: true
-      t.string :name
-      t.datetime :due_date
-      t.boolean :complete
+			t.string   :name
+			t.datetime :due_date
+			t.boolean  :complete, default: false
 
       t.timestamps
     end
