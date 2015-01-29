d42951bf5540e84d68d0edd42bb2e9048dd0d5a8
Carlos Salgado <casalgado86@yahoo.com>
2015-01-29 15:48:48 -0500

    interpret string

--- app/models/task.rb
+++ app/models/task.rb
@@ -38,8 +38,41 @@ class Task < ActiveRecord::Base
   private
 
   def interpret_string
-    self.task.name.split(" ")
-    
+    interpret = []
+    wday_array = ['d', 'l', 'm', 'c', 'j', 'v', 's']
+    array = self.name.split(" ")
+    dupli = array.dup
+    array.each do |element|
+      if element[0] == "."
+        interpret << element[1..-1]
+        dupli.delete(element)
+      end
+    end
+
+    interpret.each do |element|
+    if element.to_i == 0
+      wday_array.each_with_index do |letter, index|
+        element.gsub!(letter, "#{index}")
+      end
+      element = element.to_s
+      if element.length > 1
+        increment = element[0].to_i + ( 7 * (element.length - 1) )
+      else
+        increment = element.to_i
+      end
+      next_day = Date.today + increment - Date.today.wday # solo funciona para wday > Date.today.wday
+      @date_string = "#{next_day.year}-#{next_day.mon}-#{next_day.day}"
+    else
+      if element.to_i < 100
+        @time_string = element + ":00:00"
+      else
+        @time_string = element[0..-3] + ":" + element[-2..-1] + ":00"
+      end
+    end
+  end
+  new_date = @date_string + " " + @time_string
+  self.due_date = new_date
+  self.name = dupli.join(" ").capitalize
   end
 
 
